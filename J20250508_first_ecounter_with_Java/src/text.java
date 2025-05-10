ffeb61697d385d838714859e288af4055ccd0bea
Elysia4rjlx <2811197713@qq.com>
2025-05-10 14:22:48 +0800

    11

---
+++ J20250508_first_ecounter_with_Java/src/text.java
@@ -0,0 +1,9 @@
+public class text {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("1000");
+        System.out.println('a');
+        System.out.print("520");
+        System.out.println("1314");
+    }
+}
