6083f45f2d6241aee815986b4f36c5fec271e6a8
Elysia4rjlx <2811197713@qq.com>
2025-09-18 21:03:17 +0800

    Add files via upload

---
+++ J2025_09_18BinaryTreeOj/src/BinaryTree.java
@@ -0,0 +1,141 @@
+public class BinaryTree {
+    static class TreeNode {
+        public char val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(char val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 手动创建二叉树，返回根节点
+     */
+    public TreeNode createTree() {
+        TreeNode A = new TreeNode('A');
+        TreeNode B = new TreeNode('B');
+        TreeNode C = new TreeNode('C');
+        TreeNode D = new TreeNode('D');
+        TreeNode E = new TreeNode('E');
+        TreeNode F = new TreeNode('F');
+        TreeNode G = new TreeNode('G');
+        TreeNode H = new TreeNode('H');
+
+        A.left = B;
+        A.right = C;
+        B.left = D;
+        B.right = E;
+        C.left = F;
+        C.right = G;
+        E.right = H;
+
+        return A;
+    }
+
+    // 前序遍历
+    void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    ;
+
+    // 中序遍历
+    void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+
+    ;
+
+    // 后序遍历
+    void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    /**
+     * 判断一棵二叉树是否与另一颗二叉树的子树相同
+     * @param root
+     * @param subRoot
+     * @return
+     */
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q == null || p == null && q != null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+
+    }
+
+    /**
+     * 翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /**
+     * z子问题
+     * @param root
+     * @return
+     */
+    public TreeNode invertTreeSecond(TreeNode root){
+        if (root == null){
+            return null;
+        }
+        TreeNode leftT = invertTree(root.left);
+        TreeNode rightT = invertTree(root.right);
+
+        if (leftT == null && rightT == null){
+            return root;
+        }
+
+        root.right = rightT;
+        root.left = leftT;
+        return root;
+    }
+}
